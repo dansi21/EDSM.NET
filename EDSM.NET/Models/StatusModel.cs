bb0148da6c2e51e2f951e8c04d536827cb1468f6
William Lathrop <willguy21@gmail.com>
2025-04-23 21:58:46 -0700

    Initial Commit - First Class

---
+++ EDSM.NET/Models/StatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EDSM.NET.Models;
+
+public class StatusModel
+{
+    public DateTime LastUpdate { get; set; }
+    public required string Type { get; set; }
+    public required string Message { get; set; }
+    public int Status { get; set; }
+}
---
+++ EDSM.NET/Status.cs
@@ -0,0 +1,24 @@
+﻿using EDSM.NET.Models;
+using System.Net.Http.Json;
+
+namespace EDSM.NET
+{
+    public class Status
+    {
+        public async static Task<StatusModel> GetStatus() 
+        {
+            HttpClient client = new();
+            await client.GetFromJsonAsync<StatusModel>("https://www.edsm.net/api-v1/status").ContinueWith(task =>
+            {
+                if (task.IsCompletedSuccessfully)
+                {
+                    return task.Result;
+                }
+                else
+                {
+                    throw new Exception("Failed to get status from EDSM API");
+                }
+            });
+        }
+    }
+}
